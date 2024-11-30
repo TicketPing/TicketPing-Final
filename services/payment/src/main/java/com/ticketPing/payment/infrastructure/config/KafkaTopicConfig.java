5b7a821a3f08b655ed89f9e9dd7a5232ca7bf74c
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-12-01 01:10:25 +0900

    [refactor] Kafka 클러스터 설정 수정

---
+++ services/payment/src/main/java/com/ticketPing/payment/infrastructure/config/KafkaTopicConfig.java
@@ -0,0 +1,28 @@
+package com.ticketPing.payment.infrastructure.config;
+
+import messaging.topics.PaymentTopic;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic paymentCreatedTopic() {
+        return TopicBuilder.name(PaymentTopic.CREATED.getTopic())
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentCompletedTopic() {
+        return TopicBuilder.name(PaymentTopic.COMPLETED.getTopic())
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+
+}
