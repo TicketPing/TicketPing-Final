7afb7f951f9eabacb60ac0cfae7aec51663ebc49
mii2026 <mii2026@skku.edu>
2024-12-20 21:33:40 +0900

    [refact #74] 공연, 공연장, 스케줄, 좌석 가격 ERD 수정

--- services/performance/src/main/java/com/ticketPing/performance/domain/DataInitializer.java
+++ services/performance/src/main/java/com/ticketPing/performance/domain/DataInitializer.java
@@ -1,10 +1,8 @@
 package com.ticketPing.performance.domain;
 
 import com.ticketPing.performance.domain.model.entity.*;
-import com.ticketPing.performance.domain.model.enums.SeatRate;
 import com.ticketPing.performance.domain.repository.PerformanceHallRepository;
 import com.ticketPing.performance.domain.repository.PerformanceRepository;
-import com.ticketPing.performance.domain.repository.ScheduleRepository;
 import com.ticketPing.performance.domain.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -21,7 +19,6 @@ public class DataInitializer implements CommandLineRunner {
 
     private final PerformanceRepository performanceRepository;
     private final PerformanceHallRepository performanceHallRepository;
-    private final ScheduleRepository scheduleRepository;
     private final SeatRepository seatRepository;
 
     @Override
@@ -32,46 +29,64 @@ public class DataInitializer implements CommandLineRunner {
         }
 
         // 공연장 더미 데이터 생성
-        PerformanceHall hall1 = PerformanceHall.createTestData("국립극장", "서울특별시 남산동 1-1", 800, 10, 5);
+        PerformanceHall hall1 = PerformanceHall.createTestData("국립극장", "서울특별시 남산동 1-1", 50, 10, 5);
         performanceHallRepository.save(hall1);
 
         PerformanceHall hall2 = PerformanceHall.createTestData("세종문화회관", "서울특별시 세종로 81", 1200, 40, 30);
         performanceHallRepository.save(hall2);
 
         // 공연 더미 데이터 생성
-        Performance performance1 = Performance.createTestData("햄릿", LocalDateTime.now().minusDays(10), LocalDateTime.now().plusDays(10),
-            LocalDate.now().minusDays(5), LocalDate.now().plusDays(10), 19, UUID.randomUUID());
+        Performance performance1 = Performance.createTestData(
+                "햄릿",
+                "https://fastly.picsum.photos/id/53/250/400.jpg?hmac=XvJLere1krZjwF7Vy_J4hZpWHL9R-Ic6Hup4lQb62Yw",
+                120,
+                LocalDateTime.now().minusDays(10),
+                LocalDateTime.now().plusDays(10),
+                LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(10),
+                19,
+                UUID.randomUUID(),
+                hall1);
         performance1 = performanceRepository.save(performance1);
 
-        Performance performance2 = Performance.createTestData("라이온 킹", LocalDateTime.now().minusDays(5), LocalDateTime.now().plusDays(15),
-            LocalDate.now().minusDays(3), LocalDate.now().plusDays(15), 12, UUID.randomUUID());
+        Performance performance2 = Performance.createTestData(
+                "라이온 킹",
+                "https://fastly.picsum.photos/id/110/250/400.jpg?hmac=zs2-YrgHbuu-kSuG4dw1bynTUoD6VpOEJguE3j9NOtQ",
+                120,
+                LocalDateTime.now().plusDays(5),
+                LocalDateTime.now().plusDays(15),
+                LocalDate.now().plusDays(10),
+                LocalDate.now().plusDays(15),
+                12,
+                UUID.randomUUID(),
+                hall2);
         performance2 = performanceRepository.save(performance2);
 
+        // 공연 일정 더미 데이터 생성
+        Schedule schedule1 = Schedule.createTestData(LocalDateTime.now().plusDays(5), performance1);
+        performance1.addSchedule(schedule1);
+
+        Schedule schedule2 = Schedule.createTestData(LocalDateTime.now().plusDays(10), performance2);
+        performance2.addSchedule(schedule2);
+
         // 좌석 가격 더미 데이터 생성
-        SeatCost seatCost1 = SeatCost.createTestData(SeatRate.S, 120000, performance1);
+        SeatCost seatCost1 = SeatCost.createTestData("S석", 120000, performance1);
         performance1.addSeatCost(seatCost1);
 
-        SeatCost seatCost2 = SeatCost.createTestData(SeatRate.A, 90000, performance1);
+        SeatCost seatCost2 = SeatCost.createTestData("A석", 90000, performance1);
         performance1.addSeatCost(seatCost2);
 
-        SeatCost seatCost3 = SeatCost.createTestData(SeatRate.B, 60000, performance1);
+        SeatCost seatCost3 = SeatCost.createTestData("B석", 60000, performance1);
         performance1.addSeatCost(seatCost3);
 
-        SeatCost seatCost4 = SeatCost.createTestData(SeatRate.S, 150000, performance2);
+        SeatCost seatCost4 = SeatCost.createTestData("S석", 150000, performance2);
         performance2.addSeatCost(seatCost4);
 
-        SeatCost seatCost5 = SeatCost.createTestData(SeatRate.A, 110000, performance2);
+        SeatCost seatCost5 = SeatCost.createTestData("A석", 110000, performance2);
         performance2.addSeatCost(seatCost5);
 
-        SeatCost seatCost6 = SeatCost.createTestData(SeatRate.B, 80000, performance2);
+        SeatCost seatCost6 = SeatCost.createTestData("B석", 80000, performance2);
         performance2.addSeatCost(seatCost6);
-
-        // 공연 일정 더미 데이터 생성
-        Schedule schedule1 = Schedule.createTestData(LocalDateTime.now().plusDays(1), hall1, performance1);
-        scheduleRepository.save(schedule1);
-
-        Schedule schedule2 = Schedule.createTestData(LocalDateTime.now().plusDays(2), hall2, performance2);
-        scheduleRepository.save(schedule2);
 
         // 좌석 더미 데이터 생성
         createSeats(schedule1);
@@ -79,10 +94,13 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void createSeats(Schedule schedule) {
-        for (int row = 1; row <= schedule.getPerformanceHall().getRows(); row++) {
-            for (int column = 1; column <= schedule.getPerformanceHall().getColumns(); column++) {
-                SeatCost seatCost = determineSeatCost(schedule.getPerformance(), row, schedule.getPerformanceHall().getRows());
-                Seat seat = Seat.createTestData(row, column, false, seatCost,schedule);
+        Performance performance = schedule.getPerformance();
+        PerformanceHall performanceHall = performance.getPerformanceHall();
+
+        for (int row = 1; row <= performanceHall.getRows(); row++) {
+            for (int column = 1; column <= performanceHall.getColumns(); column++) {
+                SeatCost seatCost = determineSeatCost(performance, row, performanceHall.getRows());
+                Seat seat = Seat.createTestData(row, column, false, seatCost, schedule);
                 seatRepository.save(seat);
             }
         }
