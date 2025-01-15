22941a6cc17126acf49bbbbbdcaf7718b56d17ae
mii2026 <mii2026@skku.edu>
2025-01-15 21:47:22 +0900

    [feat #129] Order 서비스 feignclient 서킷 브레이커 추가

--- services/order/src/main/java/com/ticketPing/order/infrastructure/client/PerformanceFeignClient.java
+++ services/order/src/main/java/com/ticketPing/order/infrastructure/client/PerformanceFeignClient.java
@@ -2,7 +2,10 @@ package com.ticketPing.order.infrastructure.client;
 
 
 import com.ticketPing.order.application.client.PerformanceClient;
+import com.ticketPing.order.infrastructure.config.CustomFeignConfig;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import performance.OrderSeatResponse;
@@ -10,17 +13,31 @@ import response.CommonResponse;
 
 import java.util.UUID;
 
-@FeignClient(name = "performance")
+import static com.ticketPing.order.common.utils.FeignFallbackUtils.handleFallback;
+
+@FeignClient(name = "performance", configuration = CustomFeignConfig.class)
 public interface PerformanceFeignClient extends PerformanceClient {
 
     @GetMapping("/api/v1/client/seats/{seatId}/order-info")
+    @CircuitBreaker(name = "performanceServiceCircuitBreaker", fallbackMethod = "fallbackForGetOrderInfo")
     ResponseEntity<CommonResponse<OrderSeatResponse>> getOrderInfo(@RequestHeader("X_USER_ID") UUID userId,
                                                                    @RequestParam("scheduleId") UUID scheduleId,
                                                                    @PathVariable("seatId") UUID seatId);
 
     @PostMapping("/api/v1/client/seats/{seatId}/extend-ttl")
+    @CircuitBreaker(name = "performanceServiceCircuitBreaker", fallbackMethod = "fallbackForExtendTTL")
     ResponseEntity<CommonResponse<Object>> extendPreReserveTTL(@RequestParam("scheduleId") UUID scheduleId,
                                                                @PathVariable("seatId") UUID seatId);
 
+    default ResponseEntity<CommonResponse<OrderSeatResponse>> fallbackForGetOrderInfo(UUID userId, UUID scheduleId, UUID seatId, Throwable cause) {
+        handleFallback(cause);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null);
+    }
+
+    default ResponseEntity<CommonResponse<Object>> fallbackForExtendTTL(UUID scheduleId, UUID seatId, Throwable cause) {
+        handleFallback(cause);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null);
+    }
 }
+
 
